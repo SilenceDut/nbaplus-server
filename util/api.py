bbd4add62b6058549481627a37e9b97ec69bffc9
SilenceDutchman <silencedutchman@foxmail.com>
2015-12-29 18:06:24 +0800

    modified api file

--- util/api.py
+++ util/api.py
@@ -1,8 +1,7 @@
 __author__ = 'silencedut'
-
+#restful api
 newsupdate='/api/v1.0/<string:type>/update'
 newsloadmore='/api/v1.0/<string:type>/loadmore/<string:newsid>'
-
 perstat='/api/v1.0/nbastat/perstat/<string:perstat>'
 teamsort='/api/v1.0/teamsort/sort'
 gamesdate='/api/v1.0/gamesdate/<string:date>'
